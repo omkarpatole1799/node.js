8b03ead61c0229604b853ebbb5693535af90a5e6
Omkar Patole <omkarpatole1799@gmail.com>
2023-07-10 11:14:52 +0530

    refactoring

--- first/app.js
+++ first/app.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+// bodyparser
+app.use(bodyParser.urlencoded({extended: false}));
 
 // serve custom js and css files
 app.use(express.static(path.join(__dirname, "public")));
 
 // products page
-app.get("/products", function (req, res, next) {
+app.get("/products", function (req, res) {
   res.sendFile(path.join(__dirname, "views", "products.html"));
 });
 
+// add product
+app.post("/products", function (req, res) {
+  // const formValue = req.body.
+  console.log(req.body);
+  res.redirect("/");
+});
+
 // home page
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 
 // 404 page
-app.use(function (req, res, next) {
+app.use(function (req, res) {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
-app.listen(3030);
+app.listen(3000);

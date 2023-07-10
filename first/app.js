8a6a2c5e6c06901fa87f7323b8101aeebf25cc34
Omkar Patole <omkarpatole1799@gmail.com>
2023-07-10 12:09:35 +0530

    added pug template engine and converted html files to pug

--- first/app.js
+++ first/app.js
@@ -4,27 +4,34 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const products = [];
+
+// setting up templating engine
+app.set("view engine", "pug");
+
 // bodyparser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // serve custom js and css files
 app.use(express.static(path.join(__dirname, "public")));
 
 // products page
 app.get("/products", function (req, res) {
-  res.sendFile(path.join(__dirname, "views", "products.html"));
+  // res.sendFile(path.join(__dirname, "views", "products.html"));
+  res.render(path.join(__dirname, "views", "products.pug"));
 });
 
 // add product
 app.post("/products", function (req, res) {
-  // const formValue = req.body.
-  console.log(req.body);
+  products.push({ product_name: req.body.product_name });
+  console.log(products);
   res.redirect("/");
 });
 
 // home page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "home.html"));
+  // res.sendFile(path.join(__dirname, "views", "home.html"));
+  res.render(path.join(__dirname, "views", "home.pug"), { prods: products, title: 'Shop' });
 });
 
 // 404 page

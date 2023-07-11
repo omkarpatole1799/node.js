cb000965fc3c493f88bc4f1ef4bde14129c07443
Omkar Patole <omkarpatole1799@gmail.com>
2023-07-11 10:46:33 +0530

    Revert "refactoring: for the added routes -shop home error"
    This reverts commit 026b137375241ede835e64bfc04f39d4d205e121.

--- first/app.js
+++ first/app.js
@@ -4,10 +4,10 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require("./Routes/admin");
-const shopRoutes = require("./Routes/shop");
-const errorController = require('./controllers/error')
-// using ejs templating engine
+const products = [];
+
+// setting up templating engine
+// app.set("view engine", "pug");
 app.set("view engine", "ejs");
 
 // bodyparser
@@ -16,11 +16,38 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // serve custom js and css files
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(adminRoutes);
+// products page
+app.get("/products", function (req, res) {
+  // res.sendFile(path.join(__dirname, "views", "products.html"));
+  res.render(path.join(__dirname, "views", "products.ejs"), {
+    pageTitle: "add-products",
+  });
+});
 
-app.use(shopRoutes);
+// add product
+app.post("/products", function (req, res) {
+  products.push({ product_name: req.body.product_name });
+  console.log(products);
+  res.redirect("/");
+});
 
-app.use(errorController.get404Page) 
+// home page
+app.get("/", (req, res) => {
+  // res.sendFile(path.join(__dirname, "views", "home.html"));
+  res.render(path.join(__dirname, "views", "home.ejs"), {
+    prods: products,
+    title: "Shop",
+    pageTitle: "Shop",
+  });
+});
+
+// 404 page
+app.use(function (req, res) {
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render(path.join(__dirname, "views", "404.ejs"), {
+    pageTitle: "error-page",
+  });
+});
 
 app.listen(3000, function (e) {
   if (e) {

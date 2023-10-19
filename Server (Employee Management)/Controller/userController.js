52444f43182c82fca703077a7f88daa241a95e7a
Omkar Patole <omkarpatole1799@gmail.com>
2023-10-19 09:09:37 +0530

    Refactor: postLogData function

--- Server (Employee Management)/Controller/userController.js
+++ Server (Employee Management)/Controller/userController.js
@@ -1,46 +1,69 @@
 // adding user logs
-const UserLog = require('../Model/logDataModel')
+const UserLog = require('../Model/logDataModel');
 
-exports.postLogData = function (req, res) {
-    console.log(req.body)
-    const { log, projectTitle } = req.body
-    const userId = req.userId
-    UserLog.create({
-        logInfo: log,
-        projectTitle,
-        UserId: userId,
-    })
-        .then((result) => {
-            res.status(201).json({
-                message: 'successfully added log',
-                status: 201,
-            })
-        })
-        .catch((err) => console.log(err))
-}
+// exports.postLogData = function (req, res) {
+//   console.log(req.body);
+//   const { log, projectTitle } = req.body;
+//   const userId = req.userId;
+//   UserLog.create({
+//     logInfo: log,
+//     projectTitle,
+//     UserId: userId
+//   })
+//     .then((result) => {
+//       res.status(201).json({
+//         message: 'successfully added log',
+//         status: 201
+//       });
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+exports.postLogData = async (req, res) => {
+  console.log(req.body);
+  const { log, projectTitle } = req.body;
+  const userId = req.userId;
+	
+  console.log(userId, 'userid here=====================');
+
+  try {
+    const createLog = await UserLog.create({
+      logInfo: log,
+      projectTitle,
+      UserId: userId
+    });
+    console.log(createLog, '----');
+    res.status(201).json({
+      message: 'successfully added log',
+      status: 201
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // getting user log list
 exports.getLogList = function (req, res) {
-    const userId = req.userId
-    console.log(req.userId)
-    UserLog.findAll({
-        attributes: ['logInfo', 'projectTitle', 'createdAt'],
-        where: {
-            userId: Number(userId),
-        },
-        raw: true,
+  const userId = req.userId;
+  console.log(req.userId);
+  UserLog.findAll({
+    attributes: ['logInfo', 'projectTitle', 'createdAt'],
+    where: {
+      userId: Number(userId)
+    },
+    raw: true
+  })
+    .then((result) => {
+      console.log(result);
+      return res.status(200).send({
+        call: 1,
+        data: result
+      });
     })
-        .then((result) => {
-            console.log(result)
-            return res.status(200).send({
-                call: 1,
-                data: result,
-            })
-        })
-        .catch((err) => {
-            return res.status(500).send({
-                call: 0,
-                data: err,
-            })
-        })
-}
+    .catch((err) => {
+      return res.status(500).send({
+        call: 0,
+        data: err
+      });
+    });
+};

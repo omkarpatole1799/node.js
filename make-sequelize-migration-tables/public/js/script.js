e69f97e83aa3672050de0d767faf6e80878a8f4d
Omkar Patole <omkarpatole1799@gmail.com>
2023-12-26 20:59:52 +0530

    Refactoring

--- make-sequelize-migration-tables/public/js/script.js
+++ make-sequelize-migration-tables/public/js/script.js
@@ -4,112 +4,130 @@ let columnName = document.querySelector('#column-name')
 let dataType = document.querySelector('#data-type')
 let allowNull = document.querySelector('#allow-null')
 
+let num = 0
+let all = ''
+let dataTypes = ['INTEGER', 'BIGINT', 'STRING(255)']
+
 addRowBtn.addEventListener('click', function (e) {
-  e.preventDefault()
-  addNewRow()
-  update()
+	e.preventDefault()
+	addNewRow()
+	update()
 })
 
 function update() {
-  document.querySelectorAll('.delete-row-btn').forEach((btn) => {
-    btn.addEventListener('click', function (e) {
-      e.target.parentElement.parentElement.remove()
-    })
-  })
+	document.querySelectorAll('.delete-row-btn').forEach((btn) => {
+		btn.addEventListener('click', function (e) {
+			e.target.parentElement.parentElement.remove()
+		})
+	})
 }
 
-let num = 0
 function addNewRow() {
-  num++
-  document.querySelector('.tbody').insertAdjacentHTML(
-    'beforeend',
-    `
-	<tr>
-        <td><input type="text" name='row-${num}' /></td>
-        <td><input type="text" name="row-${num}" /></td>
-        <td><input type="checkbox" name="row-${num}" checked='true'/></td>
-		<td><button class='delete-row-btn' type='button'>Delete</button></td>
-    </tr>
-`
-  )
+	num++
+	document.querySelector('.tbody').insertAdjacentHTML(
+		'beforeend',
+		`
+		<tr>
+			<td><input type="text" name='row-${num}' /></td>
+			<td>
+					<select name="row-${num}">
+						${makeDatatypeDropdownOptions()}
+					</select>
+			</td>
+			<td><input type="checkbox" name="row-${num}" checked='true'/></td>
+			<td><button class='delete-row-btn' type='button'>Delete</button></td>
+		</tr>
+	`
+	)
 }
-let all = ''
 
+function makeDatatypeDropdownOptions() {
+	let optionsHtml = ''
+	for (let i = 0; i < dataTypes.length; i++) {
+		optionsHtml += `<option value=${dataTypes[i]}>${dataTypes[i]}</option>`
+	}
+	return optionsHtml
+}
 btn.addEventListener('click', function (e) {
-  e.preventDefault()
-  let tableName = document.getElementById('table-name').value
-  let obj = {}
-  let form = new FormData(document.getElementById('my-form'))
-  console.log(typeof obj, typeof form)
-  for (let [key, value] of form) {
-    if (obj[key] !== undefined) {
-      if (!Array.isArray(obj[key])) {
-        obj[key] = [obj[key]]
-      }
-      obj[key].push(value)
-    } else {
-      obj[key] = value
-    }
-  }
-  all += returnCommonSequelizeScript(tableName, obj)
-  // document.getElementById('preview-script').value = ''
-  // document.getElementById('preview-script').value = all
-  getSequelizeScript(all)
+	e.preventDefault()
+	let tableName = document.getElementById('table-name').value
+	let obj = {}
+	let form = new FormData(document.getElementById('my-form'))
+	console.log(typeof obj, typeof form)
+	for (let [key, value] of form) {
+		if (obj[key] !== undefined) {
+			if (!Array.isArray(obj[key])) {
+				obj[key] = [obj[key]]
+			}
+			obj[key].push(value)
+		} else {
+			obj[key] = value
+		}
+	}
+	all += returnCommonSequelizeScript(tableName, obj)
+	console.log(all)
+	getSequelizeScript(tableName, all)
 })
 
 function returnCommonSequelizeScript(tableName, obj) {
-  return `
-	const Sequelize = require('sequelize')	
-	const sequelize = require('../../config/db.connect.js')
+	return `
+			const Sequelize = require('sequelize')	
+			const sequelize = require('../../application/config/db.connect.js')
 
-	export const ${tableName} = sequelize.define('${tableName}', {
-		id:{ 
-			  type:Sequelize.INTEGER, 
-		    allowNull: false, 
-		    primaryKey: true, 
-		    autoIncrement: true
-		},
-		${makeTableColumns(obj)}	
-	})
-	`
+			const ${tableName} = sequelize.define('${tableName}', {
+				id:{ 
+					type:Sequelize.INTEGER, 
+					allowNull: false, 
+					primaryKey: true, 
+					autoIncrement: true
+				},
+				${makeTableColumns(obj)}	
+			})
+			module.exports = ${tableName}
+		`
 }
 
 function makeTableColumns(obj) {
-  let columns = ''
-  for (let i of Object.values(obj)) {
-    columns += makeColumns(i[0], i[1], i[2])
-  }
-  return columns
+	let columns = ''
+	for (let i of Object.values(obj)) {
+		columns += makeColumns(i[0], i[1], i[2])
+	}
+	return columns
 }
 
 function makeColumns(columnName, dataType, allowNull) {
-  return `
-        ${columnName} : {
-          type: Sequelize.${dataType.toUpperCase()}, 
-          allowNull: ${allowNull ? true : false}
-        },
-      `
+	return `
+			${columnName} : {
+				type: Sequelize.${dataType.toUpperCase()}, 
+				allowNull: ${allowNull ? true : false}
+			},
+			`
 }
 
-async function getSequelizeScript(script) {
-  let response = await fetch('/post-sequelize-script', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ script }),
-  })
-  let data = await response.json()
-  console.log(data)
-  if (data.success === 1) {
-    downloadSequelizeScript()
-  } else {
-    alert('Something went wrong')
-  }
+async function getSequelizeScript(fileName, script) {
+	let sendData = {
+		fileName,
+		script,
+	}
+	let response = await fetch('/post-sequelize-script', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(sendData),
+	})
+	let data = await response.json()
+	console.log(data)
+	// if (data.success === 1) {
+	// 	downloadSequelizeScript()
+	// } else {
+	// 	alert('Something went wrong')
+	// }
 }
+
 function downloadSequelizeScript() {
-  window.open('/get-sequelize-file')
-  setTimeout(() => {
-    location.reload()
-  }, 1000)
+	window.open('/get-sequelize-file')
+	setTimeout(() => {
+		location.reload()
+	}, 1000)
 }
--- make-sequelize-migration-tables/routes/indexRoutes.js
+++ make-sequelize-migration-tables/routes/indexRoutes.js
@@ -6,35 +6,44 @@ const path = require('path')
 router.get('/', homeController.getIndexView)
 
 router.post('/post-sequelize-script', (req, res) => {
-  fs.writeFileSync(
-    path.join(__dirname, 'public', 'created-scripts', 'migrationScript.js'),
-    req.body.script
-  )
-  res.status(201).json({
-    success: 1,
-    message: 'File has been successfully written.',
-  })
+	console.log(req.body, 'in controller')
+	let script = req.body.script
+	let fileName = req.body.fileName
+	fs.writeFile('./script.js', 'hello world!', function (err) {
+		if (err) {
+			console.log(err)
+		}
+		console.log('success')
+	})
+	// fs.writeFileSync(
+	// 	path.join(__dirname, 'public', 'created-scripts', `migrationScript.js`),
+	// 	script
+	// )
+	// res.status(201).json({
+	// 	success: 1,
+	// 	message: 'File has been successfully written.',
+	// })
 })
 
 router.get('/get-sequelize-file', (req, res) => {
-  console.log('downloading file')
-  res.download(
-    path.join(__dirname, 'public', 'created-scripts', 'migrationScript.js'),
-    function (err) {
-      if (err) {
-        console.log('error occured')
-      } else {
-        fs.unlinkSync(
-          path.join(
-            __dirname,
-            'public',
-            'created-scripts',
-            'migrationScript.js'
-          )
-        )
-      }
-    }
-  )
+	console.log('downloading file')
+	res.download(
+		path.join(__dirname, 'public', 'created-scripts', 'migrationScript.js'),
+		function (err) {
+			if (err) {
+				console.log('error occured')
+			} else {
+				fs.unlinkSync(
+					path.join(
+						__dirname,
+						'public',
+						'created-scripts',
+						'migrationScript.js'
+					)
+				)
+			}
+		}
+	)
 })
 
 module.exports = router

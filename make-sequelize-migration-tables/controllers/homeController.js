57572995b8cc6c079e3c8fab9d6f52e1f9008d60
Omkar Patole <omkarpatole1799@gmail.com>
2023-12-31 19:13:11 +0530

    Save file location changed, code refactoring.

--- make-sequelize-migration-tables/controllers/homeController.js
+++ make-sequelize-migration-tables/controllers/homeController.js
@@ -1,7 +1,42 @@
 const fs = require('fs')
+const path = require('path')
 const homeController = {
-  getIndexView: function (req, res) {
-    return res.render('index.ejs')
-  },
+	getIndexView: function (req, res) {
+		return res.render('index.ejs')
+	},
+
+	post_sequelize_script: (req, res) => {
+		console.log(req.body, 'in controller')
+		let script = req.body.script.toString()
+		let fileName = req.body.fileName
+
+		try {
+			fs.writeFileSync(`./created-scripts/${fileName}.js`, script)
+			res.status(201).json({
+				success: 1,
+				message: 'File has been successfully written.',
+			})
+		} catch (error) {
+			res.status(500).json({
+				success: 1,
+				message: 'Something went wrong',
+				data: error,
+			})
+		}
+	},
+
+	get_sequelize_script_file: (req, res) => {
+		let fileName = req.query.fileName
+		res.download(`./created-scripts/${fileName}.js`, function (err) {
+			if (err) {
+				return res.status(500).json({
+					success: 0,
+					message: 'Something went wrong.',
+				})
+			} else {
+				fs.unlinkSync(`./created-scripts/${fileName}.js`)
+			}
+		})
+	},
 }
 module.exports = homeController
--- make-sequelize-migration-tables/routes/indexRoutes.js
+++ make-sequelize-migration-tables/routes/indexRoutes.js
@@ -5,45 +5,8 @@ const fs = require('fs')
 const path = require('path')
 router.get('/', homeController.getIndexView)
 
-router.post('/post-sequelize-script', (req, res) => {
-	console.log(req.body, 'in controller')
-	let script = req.body.script
-	let fileName = req.body.fileName
-	fs.writeFile('./script.js', 'hello world!', function (err) {
-		if (err) {
-			console.log(err)
-		}
-		console.log('success')
-	})
-	// fs.writeFileSync(
-	// 	path.join(__dirname, 'public', 'created-scripts', `migrationScript.js`),
-	// 	script
-	// )
-	// res.status(201).json({
-	// 	success: 1,
-	// 	message: 'File has been successfully written.',
-	// })
-})
+router.post('/post-sequelize-script', homeController.post_sequelize_script)
 
-router.get('/get-sequelize-file', (req, res) => {
-	console.log('downloading file')
-	res.download(
-		path.join(__dirname, 'public', 'created-scripts', 'migrationScript.js'),
-		function (err) {
-			if (err) {
-				console.log('error occured')
-			} else {
-				fs.unlinkSync(
-					path.join(
-						__dirname,
-						'public',
-						'created-scripts',
-						'migrationScript.js'
-					)
-				)
-			}
-		}
-	)
-})
+router.get('/get-sequelize-file', homeController.get_sequelize_script_file)
 
 module.exports = router
